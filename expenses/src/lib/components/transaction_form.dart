8fcdd9abefd0d0f7b6f972936051360b89baf749
Cristiano Horn <cristianohorn@gmail.com>
2021-11-02 07:41:39 -0300

    adicionar padronização de cor de botão e botão de seleção de data

--- expenses/src/lib/components/transaction_form.dart
+++ expenses/src/lib/components/transaction_form.dart
@@ -43,15 +43,30 @@ class _TransactionFormState extends State<TransactionForm> {
               onSubmitted: (_) => _submitForm(),
               decoration: InputDecoration(labelText: 'Valor (R\$)'),
             ),
+            Container(
+              height: 70,
+              child: Row(children: [
+                Text('Nenhuma data selecionada!'),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Selecionar Data',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ]),
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                TextButton(
+                ElevatedButton(
                     onPressed: _submitForm,
                     child: Text(
                       "Adicionar",
                       style: TextStyle(
-                        color: Theme.of(context).primaryColor,
+                        color : Theme.of(context).textTheme.button!.color,
                       ),
                     )),
               ],
